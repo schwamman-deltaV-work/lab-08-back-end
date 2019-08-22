6a45f9af6941223b84b395b9b12c96301a4fe82d
Jonathon Schwamman <schwamman@msn.com>
2019-08-22 10:18:08 -0500

    Added table for events and weather to database

--- schema.sql
+++ schema.sql
@@ -6,3 +6,19 @@ CREATE TABLE locations (
   latitude NUMERIC(10, 7),
   longitude NUMERIC(10, 7)
 );
+
+DROP TABLE IF EXISTS weather;
+
+CREATE TABLE weather (
+  forecast VARCHAR(255),
+  time VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE events (
+  link VARCHAR(255),
+  name VARCHAR(255),
+  event_date VARCHAR(255),
+  summary VARCHAR(2000)
+);
